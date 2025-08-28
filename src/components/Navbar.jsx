5b5834ab0dfefe5bf06d0d8140b3cbf05c2e228f
matthewhalf <mezzatestamatteo0103@gmail.com>
2025-08-28 18:05:25 +0200

    fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { styles } from '../styles';
@@ -18,7 +18,6 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}>
             <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
-            <p className='text-white text-[18px] font-bold cursor-pointer flex'>MatthewHalf &nbsp; <span className='sm:block hidden '>WebDev</span> </p>
           </Link>
           <ul className='list-none hidden sm:flex flex-row gap-10 '>
             {navLinks.map((link) =>(
